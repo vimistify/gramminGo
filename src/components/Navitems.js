27cab7b2786e99f2c595cdc7c65fd2ee0d49e7fd
Mansi Ruhil <157950124+vimistify@users.noreply.github.com>
2025-03-02 11:51:37 +0530

    Add files via upload

---
+++ src/components/Navitems.js
@@ -0,0 +1,118 @@
+
+export const navItems = [
+  {
+    id: 1,
+    title: "Home",
+    path: "./",
+    cName: "nav-item",
+  },
+  {
+    id: 2,
+    title: "Product",
+    path: "./product",
+    cName: "nav-item",
+  },
+  {
+    id: 3,
+    title: "Contact",
+    path: "./contact",
+    cName: "nav-item",
+  },
+  {
+    id: 4,
+    title: "Stories",
+    path: "./our-story",
+    cName: "nav-item",
+  },
+  {
+    id: 5,
+    title: "User",
+    path: "./user",
+    cName: "nav-item",
+  },
+  {
+    id: 7,
+    title: "Feedback",
+    path: "./feedback",
+    cName: "nav-item",
+  },
+ 
+];
+
+export const productDropdown =[
+    {
+        id:1,
+        title:"Shop",
+        path:"./shop",
+        cName:"submenu-item",
+    },
+
+    {
+        id:2,
+        title:"Premium Beans",
+        path:"./premiumBeans",
+        cName:"submenu-item",
+    },
+
+    {
+        id:3,
+        title:"Expert Baristas",
+        path:"./expertbaristas",
+        cName:"submenu-item",
+    },
+
+    {
+      id:4,
+      title:"Cozy Ambiance",
+      path:"./cozyambiance",
+      cName:"submenu-item",
+  },
+];
+
+export const ourstoryDropdown = [
+  {
+    id: 1,
+    title: "about",
+    path: "./about",
+    cName: "submenu-item",
+  },
+  {
+    id: 2,
+    title: "testimonials",
+    path: "./testimonial",
+    cName: "submenu-item",
+  },
+];
+
+export const userLogoutDropdown = [
+  {
+    id: 1,
+    title: "login",
+    path: "./login",
+    cName: "submenu-item",
+  },
+  {
+    id: 2,
+    title: "register",
+    path: "./register",
+    cName: "submenu-item",
+  },
+];
+
+export const userLoginDropdown = [
+  {
+    id: 1,
+    title: "cart",
+    path: "./cart",
+    cName: "submenu-item",
+  },
+
+  {
+    id: 2,
+    title: "profile",
+    path: "./profile",
+    cName: "submenu-item",
+  },
+
+];
+
